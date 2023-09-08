ca357a665602bcf8e5ab06be294c867c04826146
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-09-08 14:20:39 -0300

    layout navlinks fix

--- app/security/wifi/page.tsx
+++ app/security/wifi/page.tsx
@@ -1,29 +1,44 @@
 "use client";
-// import axios from "axios";
-// import { useEffect, useState } from "react";
-// import Cookie from "js-cookie";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { CardBody } from "@nextui-org/react";
 
 import CardList from "@/components/cards/looking/ListItem";
+import { bearerToken } from "@/utils/bearerToken";
 
-import { fakerItems } from "@/config/site"; //
+export default function NotesPage() {
+  const API = process.env.NEXT_PUBLIC_API;
+  const [notes, setNotes] = useState([]);
+  const requestString = `${API}notes`;
 
-export default function WifiPage() {
-  // const API = process.env.NEXT_PUBLIC_API;
+  useEffect(() => {
+    const getNotes = async () => {
+      try {
+        const notes = await axios.get(requestString, bearerToken);
+        setNotes(notes.data);
+      } catch (err) {
+        alert("err");
+      }
+    };
+
+    getNotes();
+  }, []);
 
   return (
-    <CardBody className="md:gap-3 gap-4">
-      {fakerItems?.length === 0 ? (
-        <div className="h-full flex items-center justify-center w-full">
-          <h1 className="w-2/4 text-center">Comece adicionando um novo item!</h1>
-        </div>
-      ) : (
-        <>
-          {fakerItems?.map((item, index) => (
-            <CardList key={index} item={item} id={index} />
-          ))}
-        </>
-      )}
-    </CardBody>
+    <>
+      <CardBody className="md:gap-3 gap-4">
+        {notes?.length === 0 ? (
+          <div className="h-full flex items-center justify-center w-full">
+            <h1 className="w-2/4 text-center">Comece adicionando um novo item!</h1>
+          </div>
+        ) : (
+          <>
+            {notes?.map((item, index) => (
+              <CardList key={index} item={item} id={index} requestString={requestString} />
+            ))}
+          </>
+        )}
+      </CardBody>
+    </>
   );
 }
--- config/site.ts
+++ config/site.ts
@@ -1,4 +1,3 @@
-import { listItem } from "@/types";
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
@@ -6,7 +5,7 @@ export const siteConfig = {
   description: "Gerenciador de Senhas Seguras.",
 
   privatePages: [
-    { label: "Credenciais", href: "/security/" },
+    { label: "Credenciais", href: "/security" },
     { label: "Cartões", href: "/security/cards" },
     { label: "Notas", href: "/security/notes" },
     { label: "Wifi", href: "/security/wifi" },
@@ -19,72 +18,3 @@ export const siteConfig = {
     { label: "Sobre", href: "/about" },
   ],
 };
-
-export const fakerItems: listItem[] = [
-  {
-    title: "Facebook",
-    url: "https://www.facebook.com/",
-    username: "facebook_username123",
-    password: "pass123",
-  },
-  {
-    title: "Instagram",
-    url: "https://www.instagram.com/",
-    username: "insta_username456",
-    password: "instaPass789",
-  },
-  {
-    title: "Twitter",
-    url: "https://www.twitter.com/",
-    username: "twitter_username789",
-    password: "twitPass012",
-  },
-  {
-    title: "LinkedIn",
-    url: "https://www.linkedin.com/",
-    username: "linkedin_username345",
-    password: "linkPass234",
-  },
-  {
-    title: "Pinterest",
-    url: "https://www.pinterest.com/",
-    username: "pin_username567",
-    password: "pinPass567",
-  },
-  {
-    title: "Reddit",
-    url: "https://www.reddit.com/",
-    username: "reddit_username890",
-    password: "redPass890",
-  },
-  {
-    title: "Snapchat",
-    url: "https://www.snapchat.com/",
-    username: "snap_username123",
-    password: "snapPass123",
-  },
-  {
-    title: "WhatsApp",
-    url: "https://www.whatsapp.com/",
-    username: "whatsapp_username456",
-    password: "whatsPass456",
-  },
-  {
-    title: "TikTok",
-    url: "https://www.tiktok.com/",
-    username: "tiktok_username789",
-    password: "tikPass789",
-  },
-  {
-    title: "Netflix",
-    url: "https://www.netflix.com/",
-    username: "netflix_username345",
-    password: "netPass345",
-  },
-  {
-    title: "Pinterest",
-    url: "https://www.pinterest.com/",
-    username: "pin_username567",
-    password: "pinPass567",
-  },
-];
